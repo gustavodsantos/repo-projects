1852ae775c80614dfb2c6c42ae137ec6f44d2e0a
gustavodsantos <gustavojuniordos@hotmail.com>
2025-02-02 21:34:46 -0300

    criado o gerador de senhas

--- mysite/base/views.py
+++ mysite/base/views.py
@@ -1,3 +1,6 @@
+import random
+import string
+
 from django.contrib import messages
 from django.contrib.auth import login
 from django.http import HttpResponseRedirect
@@ -27,3 +30,21 @@ def registrar_usuario(request):
 
 def home(request):
     return render(request, 'base/home.html')
+
+
+def gerador_senha(request):
+    # Define os critérios padrão
+    comprimento = int(request.GET.get('length', 12))  # Comprimento padrão
+    caracteres = string.ascii_lowercase  # Letras minúsculas
+
+    if request.GET.get('uppercase'):
+        caracteres += string.ascii_uppercase
+    if request.GET.get('numbers'):
+        caracteres += string.digits
+    if request.GET.get('special'):
+        caracteres += string.punctuation
+
+    # Gerar senha
+    senha = ''.join(random.choices(caracteres, k=comprimento))
+
+    return render(request, 'base/gerador_senha.html', {'senha': senha})
