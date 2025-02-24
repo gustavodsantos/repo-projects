00c8231833099e7c637350df0c01c854686f31be
gustavodsantos <gustavojuniordos@hotmail.com>
2025-02-24 08:39:06 -0300

    ajustes nos templates

--- mysite/base/urls.py
+++ mysite/base/urls.py
@@ -5,7 +5,7 @@ from mysite.base import views
 app_name = 'base'
 
 urlpatterns = [
-    path('', views.home, name='home'),  # Página inicial
+    path('', views.sobre_min, name='sobre_min'),  # Página inicial
     path('senhas/', views.gerador_senha, name='gerador_senha'),
     path('logged_out/', views.logged_out, name='logged_out'),
     path('registrar/', views.registrar_usuario, name='registrar_usuario'),
